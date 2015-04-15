d0a70837047caa291672252aa08df4e51db238db
Rachel Bittner <rachel.bittner@nyu.edu>
2015-04-15 00:26:03 -0400

    add feature normalization and separate function for overlap and labeling

--- contour_utils.py
+++ contour_utils.py
@@ -32,7 +32,7 @@ def load_contour_data(fpath):
     return contour_data
 
 
-def features_from_contour_data(contour_data):
+def features_from_contour_data(contour_data, normalize=True):
     """ Get subset of columns corresponding to features.
     Adds labels column with all labels unset.
 
@@ -40,6 +40,8 @@ def features_from_contour_data(contour_data):
     ----------
     contour_data : DataFrame
         Pandas data frame with all contour data.
+    normalize : Bool
+        If true, performs trackwise normalization over salience.
 
     Returns
     -------
@@ -47,7 +49,34 @@ def features_from_contour_data(contour_data):
         Pandas data frame with contour feature data.
     """
     features = contour_data.iloc[:, 2:12]
+    if normalize:
+        _, _, contour_sal = contours_from_contour_data(contour_data)
+
+        #maximum salience value across all contours
+        sal_max = contour_sal.max().max()
+
+        # normalize salience features by max salience
+        features['salience mean'] = features['salience mean']/sal_max
+        features['salience std'] = features['salience std']/sal_max
+
+        # normalize saience total by max salience and duration
+        features['salience tot'] = \
+            features['salience tot']/(sal_max*features['duration'])
+
+        # compute min and max duration
+        dur_min = features['duration'].min()
+        dur_max = features['duration'].max()
+
+        # normalize duration to be between 0 and 1
+        features['duration'] = \
+            (features['duration'] - dur_min)/(dur_max - dur_min)
+
+        # give standardized duration back to total salience
+        features['salience tot'] = \
+            features['salience tot']*features['duration']
+
     features['labels'] = -1  # all labels are unset
+    features['overlap'] = -1 # overlaps are unset
     return features
 
 
@@ -99,7 +128,7 @@ def load_annotation(fpath):
     return annot_data
 
 
-def make_coverage_plot(contour_data, annot_data):
+def make_coverage_plot(contour_data, annot_data, contour_data2=None):
     """ Plot contours against annotation.
 
     Parameters
@@ -109,6 +138,15 @@ def make_coverage_plot(contour_data, annot_data):
     annot_data : DataFrame
         Pandas data frame with all annotation data.
     """
+    if contour_data2 is not None:
+        c_times2, c_freqs2, _ = contours_from_contour_data(contour_data2)
+        for (times, freqs) in zip(c_times2.iterrows(), c_freqs2.iterrows()):
+            times = times[1].values
+            freqs = freqs[1].values
+            times = times[~np.isnan(times)]
+            freqs = freqs[~np.isnan(freqs)]
+            plt.plot(times, freqs, '.c', zorder=1)
+
     c_times, c_freqs, _ = contours_from_contour_data(contour_data)
     plt.figure()
     for (times, freqs) in zip(c_times.iterrows(), c_freqs.iterrows()):
@@ -116,13 +154,14 @@ def make_coverage_plot(contour_data, annot_data):
         freqs = freqs[1].values
         times = times[~np.isnan(times)]
         freqs = freqs[~np.isnan(freqs)]
-        plt.plot(times, freqs, '.r')
-    plt.plot(annot_data['time'], annot_data['f0'], ',g')
+        plt.plot(times, freqs, '.r', zorder=2)
+
+    plt.plot(annot_data['time'], annot_data['f0'], '+k', zorder=3)
     plt.show()
 
 
-def label_contours(contour_data, annot_data, olap_thresh, regression=False):
-    """ Assign labels to contours based on annotation.
+def contour_overlap(contour_data, annot_data):
+    """ Compute contours based on annotation.
     Contours with at least olap_thresh overlap with annotation
     are labeled as positive examples. Otherwise negative.
 
@@ -137,11 +176,11 @@ def label_contours(contour_data, annot_data, olap_thresh, regression=False):
 
     Returns
     -------
-    features : DataFrame
-        Pandas data frame with features and labels.
+    feature_data : DataFrame
+        Pandas data frame with feature_data and labels.
     """
     c_times, c_freqs, _ = contours_from_contour_data(contour_data)
-    features = features_from_contour_data(contour_data)
+    feature_data = features_from_contour_data(contour_data)
 
     for (times, freqs) in zip(c_times.iterrows(), c_freqs.iterrows()):
         row_idx = times[0]
@@ -160,15 +199,32 @@ def label_contours(contour_data, annot_data, olap_thresh, regression=False):
         res = mir_eval.melody.evaluate(gt_segment['time'].values,
                                        gt_segment['f0'].values, times, freqs)
 
-        if not regression:
-            if res['Overall Accuracy'] >= olap_thresh:
-                features.ix[row_idx, 'labels'] = 1  # melody contour
-            else:
-                features.ix[row_idx, 'labels'] = 0  # non-melody contour
-        else:
-            features.ix[row_idx, 'labels'] = res['Overall Accuracy']
+        feature_data.ix[row_idx, 'overlap'] = res['Overall Accuracy']
 
-    return features
+    return feature_data
+
+
+def label_contours(feature_data, olap_thresh):
+    """ Compute contours based on annotation.
+    Contours with at least olap_thresh overlap with annotation
+    are labeled as positive examples. Otherwise negative.
+
+    Parameters
+    ----------
+    contour_data : DataFrame
+        Pandas data frame with all contour data.
+    annot_data : DataFrame
+        Pandas data frame with all annotation data.
+    olap_thresh : float
+        Overlap threshold for positive examples
+
+    Returns
+    -------
+    feature_data : DataFrame
+        Pandas data frame with feature_data and labels.
+    """
+    feature_data['labels'] = 1*(feature_data['overlap'] > olap_thresh)
+    return feature_data
 
 
 def find_overlapping_contours(contour_data, annot_data):
@@ -228,5 +284,23 @@ def join_features(features_list):
     return all_features
 
 
+def pd_to_sklearn(features):
+    """ Convert pandas data frame to sklearn style features and labels 
+
+    Parameters
+    ----------
+    features : DataFrame
+        DataFrame containing labeled features.
+
+    Returns
+    -------
+    X : np.ndarray
+        fetures (n_samples x n_features)
+    Y : np.1darray
+        Labels (n_samples,)
+    """
+    X = np.array(features.iloc[:, 0:10])
+    Y = np.array(features['labels'])
+    return X, Y
 
 
