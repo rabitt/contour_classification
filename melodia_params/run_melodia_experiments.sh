46d91255be8be2cdfb43f0ccba26ce714f23a1fe
Rachel Bittner <rachel.bittner@nyu.edu>
2015-04-28 00:38:18 -0400

    move melodia script

---
+++ melodia_params/run_melodia_experiments.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+MIXPATH="/Datasets/MedleyDB_Mixes/"
+OUTPATH="~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/"
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_00.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv $OUTPATH/nu_00/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_01.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_01/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_02.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_02/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_03.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_03/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_04.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_04/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_05.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_05/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_06.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_06/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_07.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_07/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_08.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_08/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_09.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_09/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_10.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_10/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n01.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n01/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n02.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n02/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n03.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n03/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n04.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n04/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n05.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n05/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n06.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n06/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n07.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n07/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n08.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n08/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n09.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n09/
+
+# sonic-annotator -t "mtg-melodia-nu-parameters_n10.n3" -w csv -r $MIXPATH
+# mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n10/
+
+sonic-annotator -t "mtg-melodia-nu-parameters_n005.n3" -w csv -r $MIXPATH
+mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n005/
+
+sonic-annotator -t "mtg-melodia-nu-parameters_n015.n3" -w csv -r $MIXPATH
+mv $MIXPATH/*.csv ~/Dropbox/MARL/repos/contour_classification/Melodia_Output_nu/nu_n015/
